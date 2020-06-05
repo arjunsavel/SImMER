922e70514eaf57f873d9350e241c38751af52fdc
Arjun Savel <asavel@berkeley.edu>
2020-06-05 09:54:14 -0700

    Made zoom a global function [skip ci]

--- src/simmer/plotting.py
+++ src/simmer/plotting.py
@@ -4,6 +4,20 @@ import matplotlib.pyplot as plt
 plot_config = None
 
 
+def zoom(image, zoom_scale):
+    """
+    TODO: Check that this works for odd, even zoom_scale
+    TODO: write docsting
+    TODO: write tests
+    """
+    cent_x = im_shape[0] / 2
+    cent_y = im_shape[1] / 2
+    zoomed = image[cent_x - zoom_scale / 2 : cent_x + zoom_scale / 2][
+        cent_y - zoom_scale / 2 : cent_y + zoom_scale / 2
+    ]
+    return zoomed
+
+
 def add_colorbars(fig):
     """
     TODO: not use type, as that's already defined.
@@ -62,19 +76,6 @@ def plot_array(
             )
             ax.tick_params(axis="both", which="major", labelsize=20)
         return fig, cim
-
-    def zoom(image, zoom_scale):
-        """
-        TODO: Check that this works for odd, even zoom_scale
-        TODO: write docsting
-        TODO: write tests
-        """
-        cent_x = im_shape[0] / 2
-        cent_y = im_shape[1] / 2
-        zoomed = image[cent_x - zoom_scale / 2 : cent_x + zoom_scale / 2][
-            cent_y - zoom_scale / 2 : cent_y + zoom_scale / 2
-        ]
-        return zoomed
 
     def plot_many():
         nrows = 4
