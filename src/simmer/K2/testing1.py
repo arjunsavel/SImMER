2e85c39855d140cfa958d72bb4c70f58fe21f495
Holden Gill <holdengill@Holdens-MacBook-Pro.local>
2021-02-02 16:13:42 -0800

    adding tests

--- src/simmer/K2/testing1.py
+++ src/simmer/K2/testing1.py
@@ -26,20 +26,64 @@ from CC_py import contrast_curve_3 as cc
 
 class TestContrastCurve(unittest.TestCase):
 
+    def all_same(items):
+        return all(x == items[0] for x in items)
+
     def test_constant_flux(self):
         rows,cols = (600,600)
         arr = np.array([[1000]*cols]*rows)
         result = cc(arr)
-        self.assertTrue(result[1][0] == result[1][-1])
+        self.assertTrue(all_same(result[1])
     
     def test_radius_size(self):
         #arr = ArrayClass(600,0).arr
         rows,cols = (600,600)
         arr = np.array([[1000]*cols]*rows)
-        rad2_result = cc(arr,radius_size=2)
+        rad1_result = cc(arr,radius_size=2)
         rad3_result = cc(arr,radius_size=3)
-        self.assertTrue(len(rad3_result[0]) < len(rad2_result[0]))
-        
+        rad_6_result=cc(arr,radius_size=6)
+        self.assertTrue(all([len(rad1_result[0])>len(rad3_result[0]),
+                             len(rad3_result[0])>len(rad6_result[0])]))
+    def test_output_array_dim():
+        rows,cols = (600,600)
+        arr = np.array([[1000]*cols]*rows)
+        rad1_result = cc(arr,radius_size=1)
+        rad3_result = cc(arr,radius_size=3)
+        rad6_result = cc(arr,radius_size=6)
+        self.assertTrue(all ([len(rad1_result[0])==len(rad1_result[1]),len(rad3_result[0])==len(rad3_result[1]),
+                 len(rad6_result[0])==len(rad6_result[1])]))
+    def test_pseudo_star_mag():
+        rows,cols = (600,600)
+        arr = np.array([[1]*cols]*rows)
+        arr[299:301,299:301] = 1000
+        result = cc(arr)
+        correct = [7.5] * (len(result[1])-1)
+        self.assertTrue(all([result[1][0] == 0, np.allclose(correct,result[1][1:])]))
+
+    def test_arclength():
+        rows,cols = (600,600)
+        arr = np.array([[1000]*cols]*rows)
+        result = cc(arr)
+        correct = []
+        for i in range(len(result[0])):
+            x = i*0.033 + 0.033
+            correct.append(x)
+        self.assertTrue(np.allclose(correct,result[0]))
+
+    def test_0_vals():
+        arr = np.zeros((600,600))
+        result = cc(arr)
+        correct = [np.NaN] * len(result[1])
+        self.assertTrue(all_same(correct==result[1]))
 
 if __name__ == '__main__':
     unittest.main()
+
+
+
+
+
+
+
+
+                    
