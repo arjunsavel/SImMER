ab6d1129ef9a19ffc668164ac7f9f0ea5d4568be
Arjun Savel <35353555+arjunsavel@users.noreply.github.com>
2021-06-03 14:32:54 -0700

    Fix author string

--- src/simmer/__init__.py
+++ src/simmer/__init__.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __uri__ = "https://simmer.readthedocs.io"
-__author__ = (
-    "Arjun B. Savel, Lea Hirsch, Courtney D. Dressing, David R. Ciardi"
-)
+__author__ = "Arjun B. Savel, Lea Hirsch, Courtney D. Dressing, David R. Ciardi"
 __email__ = "asavel@gmail.com"
 __license__ = "MIT"
 __version__ = "0.3.10"
