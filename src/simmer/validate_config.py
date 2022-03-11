bf2b28c234ae224bd05aa49b174a99d90c53819d
Courtney Dressing <cddressing@gmail.com>
2022-03-11 09:00:52 -0800

    Check that values in config file match values in fits headers.
    Code now checks the fits headers of all files included in the config file to make sure that the numbers match.

---
+++ src/simmer/validate_config.py
@@ -0,0 +1,77 @@
+#CDD
+# Created: 3/7/22
+# Updated: 3/7/22
+# Updated: 3/7/22
+
+"""
+Confirm that the values listed in a configuration file match the values in the FITS file headers.
+"""
+
+import numpy as np
+import pandas as pd
+import astropy.io.fits as pyfits
+import os as os
+
+
+from . import utils as u
+
+def validate_config(config, raw_dir, inst):
+    print('inspecting directory: ', raw_dir)
+
+    objissues = 0
+    expissues = 0
+    filtissues = 0
+    for ii in np.arange(len(config)):
+        #work through each line of config file
+        this = config.iloc[ii]
+        flist = u.make_filelist(raw_dir, eval(this.Filenums), inst)
+        #print('this line: ', np.array(this))
+        for file in flist:
+            head = pyfits.getheader(file)
+            hkeys = np.array(list(head.keys()))
+            print('     ', os.path.basename(file), head['Object'], head['RA'], head['DEC'])
+
+            if (this.Object.casefold() != head['Object'].casefold()):
+                print('ISSUE! ', os.path.basename(file))
+                print('     Object mismatch')
+                print('     Object in header: ', head['Object'])
+                print('     Object in logsheet: ', this.Object)
+                print('     Pointing RA: ', head['RA'])
+                print('     Pointing Dec ', head['DEC'])
+                print('**************************')
+                objissues += 1
+
+            #Compare exposure times. ITIME0 is recorded in microseconds
+            if (this.ExpTime != (head['ITIME0']/1e6)):
+                print('ISSUE! ', os.path.basename(file))
+                print('     ExpTime mismatch')
+                print('     ExpTime in header: ', head['ITIME0']/1e6)
+                print('     ExpTime in logsheet: ', this.ExpTime)
+                print('**************************')
+                expissues += 1
+
+                #Check filters for everything but darks
+            if this.Object != "dark":
+                if np.logical_and(this.Filter != head['FILT1NAM'], this.Filter != 'J+Ch4-1.2'):
+                    print('ISSUE! ', os.path.basename(file))
+                    print('     Filter mismatch')
+                    print('     Filters in header: ', head['FILT1NAM'],head['FILT2NAM'])
+                    print('     Filter in logsheet: ', this.Filter)
+                    print('**************************')
+                    filtissues += 1
+
+                #Check filters for J+Ch1.4
+                if this.Filter == 'J+Ch4-1.2':
+                    if np.logical_or(head['FILT1NAM'] != 'J', head['FILT2NAM'] != 'Ch4-1.2'):
+                        print('ISSUE! ', os.path.basename(file))
+                        print('     Filter mismatch')
+                        print('     Filters in header: ', head['FILT1NAM'],head['FILT2NAM'])
+                        print('     Filter in logsheet: ', this.Filter)
+                        print('**************************')
+                        filtissues += 1
+
+    print('#################################')
+    print('SUMMARY')
+    print('Object mismatches: ', objissues)
+    print('Exptime mismatches: ', expissues)
+    print('Filter mismatches: ', filtissues)
