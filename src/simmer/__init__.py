f4102a66f922d9c5a616bc86e60d0202caee7c98
Arjun Savel <35353555+arjunsavel@users.noreply.github.com>
2022-12-02 14:18:32 -0500

    Update author list, increment version to 1.0.1 [skip ci]

--- src/simmer/__init__.py
+++ src/simmer/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
 __uri__ = "https://simmer.readthedocs.io"
-__author__ = "Arjun B. Savel, Lea Hirsch, Courtney D. Dressing, David R. Ciardi"
+__author__ = "Arjun B. Savel, Lea Hirsch, Holden Gill, Courtney D. Dressing, David R. Ciardi"
 __email__ = "asavel@gmail.com"
 __license__ = "MIT"
-__version__ = "1.0.1"
-__release__ = "1.0.1"
+__version__ = "1.0.2"
+__release__ = "1.0.2"
 __description__ = "Stellar Image Maturation via Efficient Reduction"
