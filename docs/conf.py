97231e9ef108b9967e0f550961d4aba8e3b17b4b
Arjun Savel <35353555+arjunsavel@users.noreply.github.com>
2022-12-02 14:57:11 -0500

    change docs theme [skip ci]

--- docs/conf.py
+++ docs/conf.py
@@ -142,7 +142,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_book_theme"
 # html_favicon = "_static/favicon.png"
 
 # Theme options are theme-specific and customize the look and feel of a theme
