c5c2fc2465f50ca4d98e37d2f593d934c5fa1ec7
Arjun Savel <35353555+arjunsavel@users.noreply.github.com>
2024-02-21 22:01:24 -0500

    remove setup (#166) [skip ci]
    * Update pyproject.toml [skip ci]
    
    * remove setup file! [skip ci]
    
    * remove extra info from init [skip ci]
    
    * just remove extra test line!
    
    * remove root for version?
    
    * remove coverage file [skip ci]

--- src/simmer/__init__.py
+++ src/simmer/__init__.py
@@ -1,9 +1,1 @@
-# -*- coding: utf-8 -*-
 
-__uri__ = "https://simmer.readthedocs.io"
-__author__ = "Arjun B. Savel, Lea Hirsch, Holden Gill, Courtney D. Dressing, David R. Ciardi"
-__email__ = "asavel@gmail.com"
-__license__ = "MIT"
-__version__ = "1.0.2"
-__release__ = "1.0.2"
-__description__ = "Stellar Image Maturation via Efficient Reduction"
